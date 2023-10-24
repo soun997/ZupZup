b37cd99f450961a9bc394a676bfca20416d362b9
JuHyun Kim <skydreamer210@gmail.com>
2023-10-24 18:13:12 +0900

    [S09P31A202-164] feat: spring security WebSecurityCustomizer 설정

---
+++ backend/zupzup/src/main/java/com/twoez/zupzup/config/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.twoez.zupzup.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+                .requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity, HandlerMappingIntrospector introspector)
+            throws Exception {
+        return httpSecurity.build();
+    }
+
+
+}

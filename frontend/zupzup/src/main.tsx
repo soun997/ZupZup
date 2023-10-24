5dcdfcfc7f299506fb168bd24a00a830cfe71302
kimssumin <ksumin0730@naver.com>
2023-10-24 10:36:05 +0900

    [S09P31A202-132] feat: suspense 를 활용한 loading 처리

--- frontend/zupzup/src/main.tsx
+++ frontend/zupzup/src/main.tsx
@@ -1,8 +1,9 @@
-import React, { Suspense } from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./styles/font.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './styles/font.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Loading } from 'pages/index.ts';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -12,12 +13,12 @@ const queryClient = new QueryClient({
   },
 });
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Suspense>
+    <Suspense fallback={<Loading />}>
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
     </Suspense>
-  </React.StrictMode>
+  </React.StrictMode>,
 );

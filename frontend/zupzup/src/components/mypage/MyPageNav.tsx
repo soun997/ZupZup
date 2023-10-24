2c35f3a7016b5ee340b842ebc47f336bd2bfb1fb
kimssumin <ksumin0730@naver.com>
2023-10-24 11:04:24 +0900

    [S09P31A202-136] feat: 상단 nav 바 구현

---
+++ frontend/zupzup/src/components/mypage/MyPageNav.tsx
@@ -0,0 +1,64 @@
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+
+import * as utils from 'utils';
+
+import CoinSvg from 'assets/icons/coin.svg?react';
+import ShopSvg from 'assets/icons/shop.svg?react';
+import SettingSvg from 'assets/icons/setting.svg?react';
+
+interface Props {
+  coin: number;
+}
+
+const MyPageNav = ({ coin }: Props) => {
+  const navigate = useNavigate();
+  return (
+    <S.Wrap>
+      <S.LeftSection>
+        <CoinSvg />
+        <span>{coin}</span>
+      </S.LeftSection>
+      <S.RightSection>
+        <ShopSvg onClick={() => navigate(utils.URL.MYPAGE.SHOP)} />
+        <SettingSvg onClick={() => navigate(utils.URL.SETTING.HOME)} />
+      </S.RightSection>
+    </S.Wrap>
+  );
+};
+
+const S = {
+  Wrap: styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    height: 44px;
+    justify-content: space-between;
+    padding: 0 10px;
+    overflow: hidden;
+  `,
+
+  LeftSection: styled.div`
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 4px;
+    flex-shrink: 0;
+    span {
+      color: ${({ theme }) => theme.color.gray2};
+      font-weight: ${({ theme }) => theme.font.weight.body2};
+      font-size: ${({ theme }) => theme.font.size.body2};
+      font-family: ${({ theme }) => theme.font.family.focus2};
+      margin-top: 2px;
+    }
+  `,
+
+  RightSection: styled.div`
+    cursor: pointer;
+    display: flex;
+    gap: 8px;
+  `,
+};
+
+export default MyPageNav;
--- frontend/zupzup/src/pages/mypage/MyPage.tsx
+++ frontend/zupzup/src/pages/mypage/MyPage.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import MyPageNav from 'components/mypage/MyPageNav';
+import styled from 'styled-components';
 
 const MyPage = () => {
-  return <div>MyPage</div>;
+  return (
+    <S.Wrap>
+      <MyPageNav coin={320} />
+    </S.Wrap>
+  );
+};
+
+const S = {
+  Wrap: styled.div`
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    width: 100%;
+    height: 100vh;
+    background: url('assets/images/mypage-wall.png');
+  `,
 };
 
 export default MyPage;

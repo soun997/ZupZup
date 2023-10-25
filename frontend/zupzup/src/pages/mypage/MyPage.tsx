9bb105bc5ccc780a3b532c3ae609bb945ca05ca9
kimssumin <ksumin0730@naver.com>
2023-10-25 13:18:38 +0900

    [S09P31A202-137]  fix : 이미지 경로 수정

--- frontend/zupzup/src/pages/mypage/MyPage.tsx
+++ frontend/zupzup/src/pages/mypage/MyPage.tsx
@@ -49,7 +49,7 @@ const S = {
     overflow: hidden;
     width: 100%;
     height: 100vh;
-    background: url('assets/images/mypage-wall.png');
+    background: url('/assets/images/mypage-wall.png');
   `,
   Title: styled.div`
     font-size: ${({ theme }) => theme.font.size.display1};

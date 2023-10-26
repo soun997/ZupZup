c8c56ba3ed916ad38f6b5048c5c0d1782f5ceb82
김주현 <skydreamer21@naver.com>
2023-10-27 00:27:01 +0900

    [S09P31A202-164] feat: DefaultAuthenticationEntryPoint 구현

--- backend/zupzup/src/main/java/com/twoez/zupzup/config/security/handler/DefaultAuthenticationEntryPoint.java
+++ backend/zupzup/src/main/java/com/twoez/zupzup/config/security/handler/DefaultAuthenticationEntryPoint.java
@@ -1,5 +1,52 @@
 package com.twoez.zupzup.config.security.handler;
 
-public class DefaultAuthenticationEntryPoint {
+import com.twoez.zupzup.config.security.HttpRequestEndpointChecker;
+import com.twoez.zupzup.global.exception.HttpExceptionCode;
+import com.twoez.zupzup.global.response.ErrorResponse;
+import com.twoez.zupzup.global.util.Assertion;
+import com.twoez.zupzup.global.util.ExceptionResponseWriter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.stereotype.Component;
 
+/**
+ * 인증에 대한 기본 예외 처리 핸들러
+ */
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class DefaultAuthenticationEntryPoint extends Http403ForbiddenEntryPoint {
+
+    private final HttpRequestEndpointChecker endpointChecker;
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException arg2) throws IOException {
+
+        HttpExceptionCode authenticationExceptionCode = HttpExceptionCode.SECURITY_AUTHENTICATION_EXCEPTION;
+
+        Assertion.with(request)
+                .setValidation(endpointChecker::existsEndPoint)
+                .validateOrExecute(() -> {
+                    log.info("[AuthenticationEntryPoint] {} {} 유효하지 않은 접근입니다.", request.getMethod(),
+                            request.getRequestURI());
+                    try {
+                        ExceptionResponseWriter.writeException(
+                                response,
+                                authenticationExceptionCode.getHttpStatus(),
+                                ErrorResponse.from(authenticationExceptionCode)
+                        );
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+
+        super.commence(request, response, arg2);
+    }
 }

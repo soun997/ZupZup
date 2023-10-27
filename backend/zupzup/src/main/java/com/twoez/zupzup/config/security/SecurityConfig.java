1c95dbcb30146c59d4c8f104be47b7407443248d
JuHyun Kim <skydreamer210@gmail.com>
2023-10-27 16:13:43 +0900

    [S09P31A202-164] feat: SecurityConfig cors, csrf, session, httpRequest 기본 설정

--- backend/zupzup/src/main/java/com/twoez/zupzup/config/security/SecurityConfig.java
+++ backend/zupzup/src/main/java/com/twoez/zupzup/config/security/SecurityConfig.java
@@ -1,17 +1,36 @@
 package com.twoez.zupzup.config.security;
 
+import com.twoez.zupzup.config.security.handler.DefaultAccessDeniedHandler;
+import com.twoez.zupzup.config.security.handler.DefaultAuthenticationEntryPoint;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final HttpRequestEndpointChecker httpRequestEndpointChecker;
+    private final DefaultAccessDeniedHandler accessDeniedHandler;
+    private final DefaultAuthenticationEntryPoint authenticationEntryPoint;
+
+    @Value("${CLIENT_URL}")
+    private String clientUrl;
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
@@ -20,10 +39,38 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity, HandlerMappingIntrospector introspector)
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity,
+            HandlerMappingIntrospector introspector)
             throws Exception {
-        return httpSecurity.build();
+        return httpSecurity
+                .exceptionHandling(config -> config
+                        .accessDeniedHandler(accessDeniedHandler)
+                        .authenticationEntryPoint(authenticationEntryPoint))
+                .cors(config -> config.configurationSource(corsConfigurationSource()))
+                .csrf(CsrfConfigurer::disable)
+                .authorizeHttpRequests(request ->
+                        request.requestMatchers(
+                                        new MvcRequestMatcher(introspector, "api/**"))
+                                .authenticated()
+                                .anyRequest().authenticated())
+                .sessionManagement(
+                        config -> config.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of(clientUrl));
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        // 어떤 컨트롤러(url-based)에 위의 cors 설정을 할 것인지 설정
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
 
 
 }

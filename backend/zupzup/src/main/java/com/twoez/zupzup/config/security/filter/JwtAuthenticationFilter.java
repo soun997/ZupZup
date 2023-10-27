24a7506db3702a9232792db3f5cc331a37344022
JuHyun Kim <skydreamer210@gmail.com>
2023-10-27 18:00:43 +0900

    [S09P31A202-164] feat: JwtAuthenticationFilter 구현 (미완)

---
+++ backend/zupzup/src/main/java/com/twoez/zupzup/config/security/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,29 @@
+package com.twoez.zupzup.config.security.filter;
+
+import com.twoez.zupzup.config.security.jwt.JwtProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+
+
+
+    }
+}
---
+++ backend/zupzup/src/main/java/com/twoez/zupzup/config/security/jwt/JwtProvider.java
@@ -0,0 +1,9 @@
+package com.twoez.zupzup.config.security.jwt;
+
+/**
+ * 인증 완료 후 유저에 대한 token을 발급해주는 Provider
+ */
+public class JwtProvider {
+
+
+}

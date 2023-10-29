3848642c4f5291009e333d3e588112649ed83d4b
김주현 <skydreamer21@naver.com>
2023-10-30 02:19:29 +0900

    [S09P31A202-164] feat: JwtAuthenticationFilter 1차 구현

--- backend/zupzup/src/main/java/com/twoez/zupzup/config/security/filter/JwtAuthenticationFilter.java
+++ backend/zupzup/src/main/java/com/twoez/zupzup/config/security/filter/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -22,8 +23,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
             FilterChain filterChain) throws ServletException, IOException {
+        log.info("[Filter - START] JwtAuthFilter - {}", request.getRequestURI());
+        getTokenFromHeader(request).ifPresent(System.out::println);
+        doFilter(request, response, filterChain);
+        log.info("[Filter - END] JwtAuthFilter");
 
+    }
 
-
+    private Optional<String> getTokenFromHeader(HttpServletRequest request) {
+        return Optional.ofNullable(request.getHeader(AUTHORIZATION_HEADER));
     }
 }

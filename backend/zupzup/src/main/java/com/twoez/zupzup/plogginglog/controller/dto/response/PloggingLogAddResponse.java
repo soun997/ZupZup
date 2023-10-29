13d087d24317e18208a12e81cd3d846693ab42d7
sypark <soun997@gmail.com>
2023-10-30 04:26:59 +0900

    [S09P31A202-198] feature: 플로깅 등록 후에 반환할 DTO 구현

---
+++ backend/zupzup/src/main/java/com/twoez/zupzup/plogginglog/controller/dto/response/PloggingLogAddResponse.java
@@ -0,0 +1,11 @@
+package com.twoez.zupzup.plogginglog.controller.dto.response;
+
+import com.twoez.zupzup.plogginglog.domain.PloggingLog;
+
+public record PloggingLogAddResponse(Long id) {
+
+    public static PloggingLogAddResponse from(PloggingLog ploggingLog) {
+
+        return new PloggingLogAddResponse(ploggingLog.getId());
+    }
+}

3708cda02b286d78ccaa57397dc3083955e79920
김주현 <skydreamer21@naver.com>
2023-10-30 02:19:05 +0900

    [S09P31A202-164] feat: JwtProvider 1차 구현

--- backend/zupzup/src/main/java/com/twoez/zupzup/config/security/jwt/JwtProvider.java
+++ backend/zupzup/src/main/java/com/twoez/zupzup/config/security/jwt/JwtProvider.java
@@ -1,9 +1,26 @@
 package com.twoez.zupzup.config.security.jwt;
 
+import java.security.Key;
+import org.springframework.stereotype.Component;
+
 /**
  * 인증 완료 후 유저에 대한 token을 발급해주는 Provider
  */
+@Component
 public class JwtProvider {
+
+    private static final String GRANT_TYPE = "Bearer";
+
+    private final Key secretKey;
+    private final Integer accessExpiredMin;
+    private final Integer refreshExpiredDay;
+
+    public JwtProvider(JwtProperty jwtProperty) {
+        this.secretKey = jwtProperty.getKey();
+        this.accessExpiredMin = jwtProperty.getAccessExpiredMin();
+        this.refreshExpiredDay = jwtProperty.getRefreshExpiredDay();
+    }
+
 
 
 }

d809b6596127fd5aea210f0aac6606f042d8a0f1
sypark <soun997@gmail.com>
2023-10-30 06:46:43 +0900

    [S09P31A202-198] refactor: Validation 추가

--- backend/zupzup/src/main/java/com/twoez/zupzup/plogginglog/controller/dto/request/PloggingLogRequest.java
+++ backend/zupzup/src/main/java/com/twoez/zupzup/plogginglog/controller/dto/request/PloggingLogRequest.java
@@ -3,18 +3,20 @@ package com.twoez.zupzup.plogginglog.controller.dto.request;
 
 import com.twoez.zupzup.member.domain.Member;
 import com.twoez.zupzup.plogginglog.domain.PloggingLog;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 public record PloggingLogRequest(
-        @NotNull Integer distance,
+        @NotNull @Min(0) Integer distance,
         @NotNull LocalDateTime startDateTime,
         @NotNull LocalDateTime endDateTime,
-        @NotNull Integer time,
-        @NotNull Integer calories,
-        @NotNull Integer gatheredTrash,
-        @NotNull Integer coin,
-        @NotNull String routeImageUrl) {
+        @NotNull @Min(0) Integer time,
+        @NotNull @Min(0) Integer calories,
+        @NotNull @Min(0) Integer gatheredTrash,
+        @NotNull @Min(0) Integer coin,
+        @NotBlank String routeImageUrl) {
 
     public PloggingLog toEntity(Member member) {
         return PloggingLog.builder()

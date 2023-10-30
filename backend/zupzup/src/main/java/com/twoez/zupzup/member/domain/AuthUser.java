8f0264493f0d952921c8aff0a9ccb02d1372d127
김주현 <skydreamer21@naver.com>
2023-10-31 07:03:18 +0900

    [S09P31A202-164] feat: OIDC 인증을 받은 클래스로 쓰이는 AuthUser 추가

---
+++ backend/zupzup/src/main/java/com/twoez/zupzup/member/domain/AuthUser.java
@@ -0,0 +1,14 @@
+package com.twoez.zupzup.member.domain;
+
+public class AuthUser {
+
+    private OauthProvider oauthProvider;
+    private String oauthAccount;
+    private String name;
+
+    public AuthUser(OauthProvider oauthProvider, String oauthAccount, String name) {
+        this.oauthProvider = oauthProvider;
+        this.oauthAccount = oauthAccount;
+        this.name = name;
+    }
+}

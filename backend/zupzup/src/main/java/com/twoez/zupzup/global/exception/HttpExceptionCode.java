c50d942553101dc89b4da98478c50fcbad174b98
김주현 <skydreamer21@naver.com>
2023-10-31 06:58:53 +0900

    [S09P31A202-164] feat: JWT 관련 HttpExceptionCode 추가

--- backend/zupzup/src/main/java/com/twoez/zupzup/global/exception/HttpExceptionCode.java
+++ backend/zupzup/src/main/java/com/twoez/zupzup/global/exception/HttpExceptionCode.java
@@ -14,7 +14,13 @@ public enum HttpExceptionCode {
 
     SECURITY_AUTHORIZATION_EXCEPTION(HttpStatus.FORBIDDEN, "ERR_AUTH_001", "사용자 인가에 실패했습니다."),
     SECURITY_AUTHENTICATION_EXCEPTION(HttpStatus.FORBIDDEN, "ERR_AUTH_002", "사용자 인증에 실패했습니다."),
-    OAUTH_PROVIDER_NOT_FOUND(HttpStatus.NOT_FOUND, "ERR_AUTH_003", "Oauth Provider 가 존재하지 않습니다.");
+    OAUTH_PROVIDER_NOT_FOUND(HttpStatus.NOT_FOUND, "ERR_AUTH_003", "Oauth Provider 가 존재하지 않습니다."),
+
+    JWT_EXPIRED(HttpStatus.UNAUTHORIZED, "ERR_JWT_001", "JWT 기한이 만료되었습니다."),
+    JWT_MALFORMED(HttpStatus.UNAUTHORIZED, "ERR_JWT_002", "JWT가 손상되었습니다."),
+    JWT_UNSUPPORTED(HttpStatus.UNAUTHORIZED, "ERR_JWT_003", "지원되지 않는 JWT 입니다."),
+    JWT_INVALID_SIGNATURE(HttpStatus.UNAUTHORIZED, "ERR_JWT_004", "signature가 유효하지 않습니다."),
+    JWT_NOT_FOUND(HttpStatus.UNAUTHORIZED, "ERR_JWT_005", "JWT를 찾을 수 없습니다.");
 
 
 

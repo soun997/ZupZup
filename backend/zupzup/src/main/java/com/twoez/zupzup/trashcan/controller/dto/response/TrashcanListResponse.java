0c5131dcede8f4e8650a0c8cfc1d0f3dfddd92fc
안나 <dksek3050@gmail.com>
2023-10-31 14:23:10 +0900

    [S09P31A202-186] feature: ResponseDto에 guName 삭제

--- backend/zupzup/src/main/java/com/twoez/zupzup/trashcan/controller/dto/response/TrashcanListResponse.java
+++ backend/zupzup/src/main/java/com/twoez/zupzup/trashcan/controller/dto/response/TrashcanListResponse.java
@@ -10,15 +10,13 @@ import java.math.BigDecimal;
 public record TrashcanListResponse(
      BigDecimal latitude, // 위도
      BigDecimal longitude, // 경도
-     String trashcanType,
-     String guName
+     String trashcanType
 ) {
     public static TrashcanListResponse from(Trashcan trashcan) {
         return TrashcanListResponse.builder()
                 .latitude(trashcan.getLatitude())
                 .longitude(trashcan.getLongitude())
                 .trashcanType(trashcan.getTrashcanType())
-                .guName(trashcan.getGuName())
                 .build();
     }
 }

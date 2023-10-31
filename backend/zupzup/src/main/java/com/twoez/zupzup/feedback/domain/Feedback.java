c90c4edc45eb76df87b198df2a643a0cdbfdd3c5
soyun <soun997@gmail.com>
2023-10-31 17:19:56 +0900

    [S09P31A202-197] fix: Feedback 엔티티 누락된 컬럼 추가

--- backend/zupzup/src/main/java/com/twoez/zupzup/feedback/domain/Feedback.java
+++ backend/zupzup/src/main/java/com/twoez/zupzup/feedback/domain/Feedback.java
@@ -1,7 +1,11 @@
 package com.twoez.zupzup.feedback.domain;
 
 
+import com.twoez.zupzup.global.audit.BaseTime;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -10,18 +14,33 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Feedback {
+public class Feedback extends BaseTime {
 
-    @Id private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "feedback_id")
+    private Long id;
 
+    @Column(nullable = false)
     private String content;
+
+    @Column(nullable = false)
+    private Boolean isDeleted;
 
     public Feedback(String content) {
         this.content = content;
+        this.isDeleted = false;
     }
 
     public Feedback(Long id, String content) {
         this.id = id;
         this.content = content;
+        this.isDeleted = false;
+    }
+
+    public Feedback(Long id, String content, Boolean isDeleted) {
+        this.id = id;
+        this.content = content;
+        this.isDeleted = isDeleted;
     }
 }

82e6b082dbcd97ff4a458f51f5503c38bdd9326f
JuHyun Kim <skydreamer210@gmail.com>
2023-11-01 11:11:03 +0900

    [S09P31A202-164] comment: jwtAuthentication 성공 처리 로직에 대한 주석 추가

--- backend/zupzup/src/main/java/com/twoez/zupzup/config/security/handler/Oauth2SuccessHandler.java
+++ backend/zupzup/src/main/java/com/twoez/zupzup/config/security/handler/Oauth2SuccessHandler.java
@@ -30,8 +30,13 @@ public class Oauth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler{
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
             Authentication authentication) throws IOException, ServletException {
-        // TODO 1: authToken 발급
-        // TODO 2: /login-success 로 Redirect
+        // 1. authToken 발급
+        // 2. /login-success 로 Redirect
+        // ** 원래는 idToken을 프론트에서 받아와서 넘겨주면서 백에서 idToken, Provider를 받아서 처리하는 방식
+        // ** 그런데 spring security를 활용하기 때문에 url에 accessToken과 refreshToken을 노출시키지 않기 위해서 임시 토큰인 authToken 발급
+        // ** 이 때 authToken 안에는 Provider로 부터 발급받은 idToken이 payload에 있다.
+        // ** url로 밖에 전달할 수 없는 이유는 Redirect이기 때문
+        // ** 프론트의 요청을 받기 위해 authToken과 provider를 redirect로 넘겨주고 다시 받아 로그인 및 회원가입 처리
 
         log.info("onAuthenticationSuccess called");
         OidcUser oidcUser = (OidcUser) authentication.getPrincipal();

9eee4615a176d95c4c46dce769709597b0ae82c4
soyun <soun997@gmail.com>
2023-11-01 10:32:32 +0900

    [S09P31A202-197] fix: 누락된 @RequestBody annotation 추가

--- backend/zupzup/src/main/java/com/twoez/zupzup/feedback/controller/FeedbackController.java
+++ backend/zupzup/src/main/java/com/twoez/zupzup/feedback/controller/FeedbackController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +24,7 @@ public class FeedbackController {
     private final FeedbackService feedbackService;
 
     @PostMapping
-    public ApiResponse<?> feedbackAdd(@Validated FeedbackAddRequest feedbackAddRequest) {
+    public ApiResponse<?> feedbackAdd(@Validated @RequestBody FeedbackAddRequest feedbackAddRequest) {
 
         feedbackService.add(feedbackAddRequest.toEntity());
         return ApiResponse.created().build();

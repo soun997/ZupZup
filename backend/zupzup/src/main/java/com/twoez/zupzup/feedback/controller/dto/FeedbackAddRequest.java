387d02aeb53ff9471ffad4e8a0d4d179ead4d273
soyun <soun997@gmail.com>
2023-11-01 10:27:34 +0900

    [S09P31A202-197] fix: 누락된 validation annotation 추가

--- backend/zupzup/src/main/java/com/twoez/zupzup/feedback/controller/dto/FeedbackAddRequest.java
+++ backend/zupzup/src/main/java/com/twoez/zupzup/feedback/controller/dto/FeedbackAddRequest.java
@@ -2,8 +2,9 @@ package com.twoez.zupzup.feedback.controller.dto;
 
 
 import com.twoez.zupzup.feedback.domain.Feedback;
+import jakarta.validation.constraints.NotBlank;
 
-public record FeedbackAddRequest(String content) {
+public record FeedbackAddRequest(@NotBlank String content) {
 
     public Feedback toEntity() {
         return new Feedback(content);

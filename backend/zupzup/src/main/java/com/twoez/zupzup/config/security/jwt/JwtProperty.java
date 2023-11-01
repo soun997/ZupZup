e000dcc39d67357ef3d8dc76e9837764fd7ed07f
JuHyun Kim <skydreamer210@gmail.com>
2023-11-01 17:01:41 +0900

    [S09P31A202-164] fix: Jwt key 설정 방법 변경

--- backend/zupzup/src/main/java/com/twoez/zupzup/config/security/jwt/JwtProperty.java
+++ backend/zupzup/src/main/java/com/twoez/zupzup/config/security/jwt/JwtProperty.java
@@ -1,12 +1,10 @@
 package com.twoez.zupzup.config.security.jwt;
 
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.io.Encoders;
 import io.jsonwebtoken.security.Keys;
-import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import lombok.Getter;
 import lombok.Setter;
+import org.bouncycastle.util.encoders.Base64;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,11 +22,6 @@ public class JwtProperty {
 
     @Bean
     public Key getKey() {
-        String encodedSecretKey = encodeBase64SecretKey(secretKey);
-        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(encodedSecretKey));
-    }
-
-    private String encodeBase64SecretKey(String secretKey) {
-        return Encoders.BASE64.encode(secretKey.getBytes(StandardCharsets.UTF_8));
+        return Keys.hmacShaKeyFor(Base64.encode(secretKey.getBytes()));
     }
 }

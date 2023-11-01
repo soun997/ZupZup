26be0e32a1b7e08457c9e3be4f05796df72eac0a
soyun <soun997@gmail.com>
2023-11-01 12:53:57 +0900

    [S09P31A202-246] feature: repository scan 범위 설정을 위한 config 클래스 구현

---
+++ backend/zupzup/src/main/java/com/twoez/zupzup/config/jpa/JpaConfig.java
@@ -0,0 +1,14 @@
+package com.twoez.zupzup.config.jpa;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.twoez.zupzup", excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASPECTJ, pattern = "com.twoez.zupzup.*.repository.mongo.*"),
+        @ComponentScan.Filter(type = FilterType.ASPECTJ, pattern = "com.twoez.zupzup.*.repository.redis.*")})
+public class JpaConfig {
+
+}
---
+++ backend/zupzup/src/main/java/com/twoez/zupzup/config/redis/RedisConfig.java
@@ -0,0 +1,11 @@
+package com.twoez.zupzup.config.redis;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories(basePackages = "com.twoez.zupzup.plogging.repository.redis")
+public class RedisConfig {
+
+}

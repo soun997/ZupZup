7568fd9fb965b5fe2a1e72294eb661452767864d
kimssumin <ksumin0730@naver.com>
2023-11-02 10:53:56 +0900

    [S09P31A202-256] feature: 상세 페이지 내용 추가

--- frontend/zupzup/src/components/shop/FoodDetail.tsx
+++ frontend/zupzup/src/components/shop/FoodDetail.tsx
@@ -6,8 +6,9 @@ const FoodDetail = ({ foodDetail }: { foodDetail: FoodDetail }) => {
     <S.Wrap>
       <S.Image src={foodDetail.image} />
       <S.Title>{foodDetail.name}</S.Title>
-      <S.SubTitle>{foodDetail.coin} 코인</S.SubTitle>
+      <S.Content>지금까지 이런 소시지는 없었다. 이것은..더보기</S.Content>
       <S.Body>능력치 : 캐릭터의 성장도 + {foodDetail.exp}</S.Body>
+      <S.SubTitle>$ {foodDetail.coin} 코인</S.SubTitle>
     </S.Wrap>
   );
 };
@@ -19,8 +20,7 @@ const S = {
     align-items: center;
   `,
   Image: styled.img`
-    width: 80%;
-    height: 80%;
+    width: 75%;
   `,
 
   Title: styled.div`
@@ -32,13 +32,20 @@ const S = {
     color: ${({ theme }) => theme.color.main};
     font-size: ${({ theme }) => theme.font.size.focus1};
     font-family: ${({ theme }) => theme.font.family.focus1};
-    margin-top: 10px;
+    margin-top: 2px;
   `,
   Body: styled.div`
     color: ${({ theme }) => theme.color.gray2};
-    font-size: ${({ theme }) => theme.font.size.body3};
+    font-size: ${({ theme }) => theme.font.size.body2};
     font-family: ${({ theme }) => theme.font.family.focus1};
-    margin: 20px 0;
+    margin: 12px 0;
+  `,
+
+  Content: styled.div`
+    color: ${({ theme }) => theme.color.gray2};
+    font-size: ${({ theme }) => theme.font.size.body3};
+    font-family: ${({ theme }) => theme.font.family.body1};
+    margin: 10px 0;
   `,
 };
 

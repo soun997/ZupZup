dd6f93a678a8d1ddc03f08671135bb73fa9229b5
soyun <soun997@gmail.com>
2023-11-03 11:11:29 +0900

    [S09P31A202-262] feature: 사용자 캐릭터를 찾지 못했을 때의 Exception 구현 및 Handling

---
+++ backend/zupzup/src/main/java/com/twoez/zupzup/global/advice/character/CharacterControllerAdvice.java
@@ -0,0 +1,21 @@
+package com.twoez.zupzup.global.advice.character;
+
+import com.twoez.zupzup.global.advice.AdviceLoggingUtils;
+import com.twoez.zupzup.global.exception.character.CharacterNotFoundException;
+import com.twoez.zupzup.global.response.ApiResponse;
+import com.twoez.zupzup.global.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CharacterControllerAdvice {
+
+    @ExceptionHandler(CharacterNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiResponse<ErrorResponse> characterNotFound(CharacterNotFoundException e) {
+        AdviceLoggingUtils.exceptionLog(e);
+        return ApiResponse.notFound(ErrorResponse.from(e.getExceptionCode()));
+    }
+}

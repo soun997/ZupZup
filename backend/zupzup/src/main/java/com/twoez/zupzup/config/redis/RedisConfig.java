0898e02d447d12bd2bea507149cd1f104173d7fe
soyun <soun997@gmail.com>
2023-11-03 12:49:35 +0900

    [S09P31A202-266] feature: Redis @Transaction 어노테이션 사용을 위한 Config 수정

--- backend/zupzup/src/main/java/com/twoez/zupzup/config/redis/RedisConfig.java
+++ backend/zupzup/src/main/java/com/twoez/zupzup/config/redis/RedisConfig.java
@@ -1,9 +1,39 @@
 package com.twoez.zupzup.config.redis;
 
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 @Configuration
 @EnableRedisRepositories(basePackages = "com.twoez.zupzup.*.repository.redis")
-public class RedisConfig {}
+@RequiredArgsConstructor
+public class RedisConfig {
+
+    private final RedisProperties redisProperties;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+
+        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(RedisSerializer.string());
+        redisTemplate.setValueSerializer(RedisSerializer.string());
+        redisTemplate.setDefaultSerializer(RedisSerializer.string());
+        redisTemplate.setEnableTransactionSupport(true);
+
+        return redisTemplate;
+    }
+}

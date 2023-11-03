4a7f29fd5ef1a0d3a1ba82dff175a501ea16f76a
soyun <soun997@gmail.com>
2023-11-03 16:07:06 +0900

    [S09P31A202-243] refactor: CrudRepository -> RedisTemplate 사용하도록 변경

--- backend/zupzup/src/main/java/com/twoez/zupzup/plogging/repository/redis/PloggingRedisRepository.java
+++ backend/zupzup/src/main/java/com/twoez/zupzup/plogging/repository/redis/PloggingRedisRepository.java
@@ -1,9 +1,48 @@
 package com.twoez.zupzup.plogging.repository.redis;
 
-
-import com.twoez.zupzup.plogging.domain.Plogger;
-import org.springframework.data.repository.CrudRepository;
+import com.twoez.zupzup.config.redis.KeyChain;
+import com.twoez.zupzup.global.util.JsonConverter;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
+@Slf4j
+@RequiredArgsConstructor
 @Repository
-public interface PloggingRedisRepository extends CrudRepository<Plogger, String> {}
+public class PloggingRedisRepository {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    private final KeyChain keyChain;
+
+    private final Long DEFAULT = 0L;
+
+    public Long increase() {
+
+        return redisTemplate.opsForValue().increment(
+                keyChain.getKey(PloggingRedisRepository.class));
+    }
+
+    public Long decrease() {
+
+        return redisTemplate.opsForValue().increment(
+                keyChain.getKey(PloggingRedisRepository.class));
+    }
+
+    public Long findTotalPlogger() {
+        return Optional.ofNullable(redisTemplate.opsForValue().get(
+                        keyChain.getKey(PloggingRedisRepository.class)))
+                .map(total -> JsonConverter.toObject(total, Long.class))
+                .orElseGet(this::init);
+    }
+
+    private Long init() {
+
+        redisTemplate.opsForValue().set(
+                keyChain.getKey(PloggingRedisRepository.class), String.valueOf(DEFAULT));
+
+        return DEFAULT;
+    }
+}

79250d8c12c9b3f941b5ed7db2d1305882bd1a03
김주현 <skydreamer21@naver.com>
2023-11-06 01:03:55 +0900

    [S09P31A202-249] chore: dto 클래스 이름 변경

--- backend/zupzup/src/main/java/com/twoez/zupzup/member/controller/dto/RegisterMemberRequest.java
+++ backend/zupzup/src/main/java/com/twoez/zupzup/member/controller/dto/RegisterMemberRequest.java
@@ -3,5 +3,5 @@ package com.twoez.zupzup.member.controller.dto;
 
 import com.twoez.zupzup.member.domain.Gender;
 
-public record MemberHealthRequest(
+public record RegisterMemberRequest(
         Long memberId, Integer birthYear, Gender gender, Integer height, Integer weight) {}

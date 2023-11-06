d2f89f62fa1ba37b17d4cc208450ea1e6db0e305
soyun <soun997@gmail.com>
2023-11-06 16:55:43 +0900

    [S09P31A202-282] fix: Character 도메인 컬럼 어노테이션 추가

--- backend/zupzup/src/main/java/com/twoez/zupzup/character/domain/Character.java
+++ backend/zupzup/src/main/java/com/twoez/zupzup/character/domain/Character.java
@@ -5,6 +5,7 @@ import com.twoez.zupzup.global.audit.BaseTime;
 import com.twoez.zupzup.member.domain.Member;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -14,6 +15,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -25,13 +27,17 @@ public class Character extends BaseTime {
     @Column(name = "character_id")
     private Long id;
 
+    @Column(nullable = false)
     private Integer level;
 
+    @Column(nullable = false)
     private Integer exp;
 
+    @Column(nullable = false)
+    @ColumnDefault("false")
     private Boolean isDeleted;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 

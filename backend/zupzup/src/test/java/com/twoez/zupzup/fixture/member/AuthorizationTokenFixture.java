5bbddd3f1895a0a2e88f652f2c7a773083b298fb
안나 <dksek3050@gmail.com>
2023-11-07 14:52:20 +0900

    [S09P31A202-291] test: member register 헬스 정보 등록 테스트

---
+++ backend/zupzup/src/test/java/com/twoez/zupzup/fixture/member/AuthorizationTokenFixture.java
@@ -0,0 +1,37 @@
+package com.twoez.zupzup.fixture.member;
+
+import com.twoez.zupzup.config.security.jwt.AuthorizationToken;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public enum AuthorizationTokenFixture {
+    DEFAULT(
+            "ACCESSTOKEN",
+            "REFRESHTOKEN",
+            "GRANTTYPE"
+    );
+
+
+    private String accessToken;
+    private String refreshToken;
+    private String grantType;
+
+    AuthorizationTokenFixture(
+            String accessToken,
+            String refreshToken,
+            String grantType){
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.grantType = grantType;
+    }
+
+    public AuthorizationToken getAuthorizationToken(){
+        return new AuthorizationToken(
+                this.accessToken,
+                this.refreshToken,
+                this.grantType
+        );
+    }
+
+
+}
--- backend/zupzup/src/test/java/com/twoez/zupzup/member/controller/MemberControllerTest.java
+++ backend/zupzup/src/test/java/com/twoez/zupzup/member/controller/MemberControllerTest.java
@@ -2,6 +2,8 @@ package com.twoez.zupzup.member.controller;
 
 import static com.twoez.zupzup.support.docs.ApiDocumentUtils.getDocumentRequest;
 import static com.twoez.zupzup.support.docs.ApiDocumentUtils.getDocumentResponse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -9,12 +11,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.twoez.zupzup.config.security.jwt.AuthorizationToken;
+import com.twoez.zupzup.fixture.member.AuthorizationTokenFixture;
+import com.twoez.zupzup.fixture.member.MemberFixture;
 import com.twoez.zupzup.member.controller.dto.MemberHealthModifyRequest;
+import com.twoez.zupzup.member.controller.dto.MemberHealthRegisterRequest;
 import com.twoez.zupzup.member.domain.Gender;
+import com.twoez.zupzup.member.domain.Member;
 import com.twoez.zupzup.member.service.MemberService;
 import com.twoez.zupzup.support.docs.RestDocsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
@@ -22,9 +30,46 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
 
 @WebMvcTest(MemberController.class)
-public class MemberControllerTest extends RestDocsTest {
+class MemberControllerTest extends RestDocsTest {
 
     @MockBean MemberService memberService;
+
+    @Test
+    @DisplayName("사용자는 회원가입하고 헬스정보를 등록할 수 있다.")
+    void registerTest() throws Exception {
+        MemberHealthRegisterRequest request
+                = new MemberHealthRegisterRequest(
+                        1L,
+                        2000,
+                        Gender.M,
+                        180,
+                        80);
+
+        Member member = MemberFixture.DEFAULT.getMember();
+        AuthorizationToken authorizationToken = AuthorizationTokenFixture.DEFAULT.getAuthorizationToken();
+
+        given(memberService.issueAuthorizationToken(any(Long.class)))
+                .willReturn(authorizationToken);
+        given(memberService.findById(any(Long.class)))
+                .willReturn(member);
+
+        ResultActions perfrom =
+                mockMvc.perform(
+                        put("/api/v1/members/register")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(toJson(request)));
+
+        perfrom.andExpect(status().isOk())
+                .andExpect(jsonPath("$.results.memberId").value(1L))
+                .andExpect(jsonPath("$.results.name").value("줍줍이"))
+                .andExpect(jsonPath("$.results.accessToken").value("ACCESSTOKEN"))
+                .andExpect(jsonPath("$.results.refreshToken").value("REFRESHTOKEN"));
+
+        perfrom.andDo(print())
+                .andDo(document("member-register", getDocumentRequest(),getDocumentResponse()));
+    }
+
+
 
     @Test
     @DisplayName("사용자는 본인의 프로필을 조회할 수 있다.")

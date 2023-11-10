ccb955b4fcff4f5ef265d1e0694079db817c7bce
김주현 <skydreamer21@naver.com>
2023-11-10 21:51:15 +0900

    [S09P31A202-170] fix: ehcache를 위한 serializable 인터페이스 적용

--- backend/zupzup/src/main/java/com/twoez/zupzup/member/service/dto/OidcPublicKey.java
+++ backend/zupzup/src/main/java/com/twoez/zupzup/member/service/dto/OidcPublicKey.java
@@ -1,3 +1,8 @@
 package com.twoez.zupzup.member.service.dto;
 
-public record OidcPublicKey(String kid, String alg, String kty, String use, String n, String e) {}
+import java.io.Serializable;
+
+public record OidcPublicKey(String kid, String alg, String kty, String use, String n, String e)  implements
+        Serializable {
+
+}

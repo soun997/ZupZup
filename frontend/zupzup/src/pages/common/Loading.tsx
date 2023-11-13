f0d6047020cad88d05ea49ccf3b004cd20b6e670
kimssumin <ksumin0730@naver.com>
2023-11-13 09:07:23 +0900

    feature: loading page 문구 추가

--- frontend/zupzup/src/pages/common/Loading.tsx
+++ frontend/zupzup/src/pages/common/Loading.tsx
@@ -5,6 +5,10 @@ const Loading = () => {
   return (
     <S.Wrap>
       <LoadingAnimation />
+      <S.Text>데이터가 로딩중입니다</S.Text>
+      <S.SubText>
+        (로딩바가 멈추더라도 진행중이니 조금만 기다려주세요!)
+      </S.SubText>
     </S.Wrap>
   );
 };
@@ -19,6 +23,24 @@ const S = {
     height: 100dvh;
     background-color: ${({ theme }) => theme.color.background};
     color: ${({ theme }) => theme.color.dark};
+  `,
+
+  Text: styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: -30px;
+    font-weight: ${({ theme }) => theme.font.weight.focus3};
+    font-size: ${({ theme }) => theme.font.size.body2};
+    font-family: ${({ theme }) => theme.font.family.body2};
+  `,
+
+  SubText: styled.div`
+    font-weight: ${({ theme }) => theme.font.weight.body3};
+    font-size: ${({ theme }) => theme.font.size.body3};
+    font-family: ${({ theme }) => theme.font.family.body2};
+    color: ${({ theme }) => theme.color.main};
+    margin-top: 6px;
   `,
 };
 export default Loading;

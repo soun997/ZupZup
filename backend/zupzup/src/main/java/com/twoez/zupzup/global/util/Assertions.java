c9ac06aa858cd6001bc576f1279c5bd9c4f56ecc
JuHyun Kim <skydreamer210@gmail.com>
2023-11-14 17:37:52 +0900

    [S09P31A202-316] fix: class casting 하지 않도록 Assertions 로직 수정

--- backend/zupzup/src/main/java/com/twoez/zupzup/global/util/Assertions.java
+++ backend/zupzup/src/main/java/com/twoez/zupzup/global/util/Assertions.java
@@ -13,16 +13,24 @@ import java.util.function.Supplier;
  * @param <T>
  */
 public class Assertions<T> {
+
+    private static final int SUPPLIER = 0;
+    private static final int HANDLER = 1;
+
     private T param;
     private int validationId;
     private final Map<Integer, Predicate<T>> predicateMap;
-    private final Map<Integer, Object> exceptionHandlerMap;
+    private final Map<Integer, Supplier<? extends RuntimeException>> exceptionSupplierMap;
+    private final Map<Integer, Runnable> exceptionHandlerMap;
+    private final Map<Integer, Integer> typeMap;
 
     private Assertions(T param) {
         this.param = param;
         this.validationId = 1;
         predicateMap = new HashMap<>();
+        exceptionSupplierMap = new HashMap<>();
         exceptionHandlerMap = new HashMap<>();
+        typeMap = new HashMap<>();
     }
 
     public static <T> Assertions<T> with(T param) {
@@ -37,12 +45,14 @@ public class Assertions<T> {
     public <X extends RuntimeException> Assertions<T> validateOrThrow(
             Supplier<? extends X> exceptionSupplier) throws X {
         checkValidation(validationId);
-        this.exceptionHandlerMap.put(validationId++, exceptionSupplier);
+        typeMap.put(validationId, SUPPLIER);
+        this.exceptionSupplierMap.put(validationId++, exceptionSupplier);
         return this;
     }
 
     public Assertions<T> validateOrExecute(Runnable exceptionHandler) {
         checkValidation(validationId);
+        typeMap.put(validationId, HANDLER);
         this.exceptionHandlerMap.put(validationId++, exceptionHandler);
         return this;
     }
@@ -56,16 +66,15 @@ public class Assertions<T> {
     public void validate() {
         for (int i = 1; i < validationId; i++) {
             Predicate<T> predicate = predicateMap.get(i);
-            Object handler = exceptionHandlerMap.get(i);
-            if (handler instanceof Supplier) { // validateOrThrow
+            if (typeMap.get(i) == SUPPLIER) { // validateOrThrow
                 Supplier<? extends RuntimeException> exceptionSupplier =
-                        (Supplier<? extends RuntimeException>) handler;
+                        exceptionSupplierMap.get(i);
                 if (!predicate.test(param)) {
                     i = validationId;
                     throw exceptionSupplier.get();
                 }
             } else { // validateOrExecute
-                Runnable exceptionHandler = (Runnable) handler;
+                Runnable exceptionHandler = exceptionHandlerMap.get(i);
                 if (!predicate.test(param)) {
                     i = validationId;
                     exceptionHandler.run();

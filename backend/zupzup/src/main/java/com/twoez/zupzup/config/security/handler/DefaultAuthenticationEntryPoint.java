b1f78c05398377eb5b9f058da6229e009665f73e
JuHyun Kim <skydreamer210@gmail.com>
2023-11-15 15:49:12 +0900

    [S09P31A202-316] fix: Authentication Exception Handling에서 endpoint checker 제거

--- backend/zupzup/src/main/java/com/twoez/zupzup/config/security/handler/DefaultAuthenticationEntryPoint.java
+++ backend/zupzup/src/main/java/com/twoez/zupzup/config/security/handler/DefaultAuthenticationEntryPoint.java
@@ -22,8 +22,6 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class DefaultAuthenticationEntryPoint extends Http403ForbiddenEntryPoint {
 
-    private final HttpRequestEndpointChecker endpointChecker;
-
     @Override
     public void commence(
             HttpServletRequest request, HttpServletResponse response, AuthenticationException arg2)
@@ -31,10 +29,6 @@ public class DefaultAuthenticationEntryPoint extends Http403ForbiddenEntryPoint 
         log.info("[AuthenticationEntryPoint] {} {}", request.getMethod(), request.getRequestURI());
 
         Assertions.with(request)
-                // 유효한 endpoint url 체크
-                .setValidation(endpointChecker::existsEndPoint)
-                .validateOrExecute(
-                        () -> writeExceptionResponse(response, HttpExceptionCode.REQUEST_NOT_FOUND))
                 // Authorization Header가 있는지 체크
                 .setValidation(HttpRequestUtils::hasAuthorizationHeader)
                 .validateOrExecute(

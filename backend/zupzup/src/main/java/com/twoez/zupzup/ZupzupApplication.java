c9118c859c7b18b9333dc7a2c401559bc8b3a239
JuHyun Kim <skydreamer210@gmail.com>
2023-11-15 17:15:33 +0900

    [S09P31A202-316] feat: 서버 시간 설정

--- backend/zupzup/src/main/java/com/twoez/zupzup/ZupzupApplication.java
+++ backend/zupzup/src/main/java/com/twoez/zupzup/ZupzupApplication.java
@@ -1,6 +1,10 @@
 package com.twoez.zupzup;
 
 
+import jakarta.annotation.PostConstruct;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -12,4 +16,10 @@ public class ZupzupApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZupzupApplication.class, args);
     }
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("Asia/Seoul")));
+    }
+
 }

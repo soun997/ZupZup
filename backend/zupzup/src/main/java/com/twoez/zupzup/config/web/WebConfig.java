956e5d0a8727b90b33ac1f54f5f3fff62a5c8661
안나 <dksek3050@gmail.com>
2023-11-22 17:53:28 +0900

    [S09P31A202-* develop] refactor: 메소드 실행시간 측정 aop 구현

--- backend/zupzup/src/main/java/com/twoez/zupzup/config/web/WebConfig.java
+++ backend/zupzup/src/main/java/com/twoez/zupzup/config/web/WebConfig.java
@@ -3,6 +3,8 @@ package com.twoez.zupzup.config.web;
 
 import com.twoez.zupzup.config.security.jwt.AuthReqeustUserArgumentResolver;
 import java.util.List;
+
+import com.twoez.zupzup.global.aop.TimerTraceAop;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,5 +26,10 @@ public class WebConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         WebMvcConfigurer.super.addArgumentResolvers(resolvers);
         resolvers.add(authReqeustUserArgumentResolver);
+    }
+
+    @Bean
+    public TimerTraceAop timerTraceAop(){
+        return new TimerTraceAop();
     }
 }

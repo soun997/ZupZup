41fe000c3331dc69d949121a0960dbb77e4eab83
sypark <soun997@gmail.com>
2024-03-26 03:42:06 +0900

    [#1] refactor: QueryDSL repository 추상화

--- backend/zupzup/src/main/java/com/twoez/zupzup/pet/repository/PetQueryRepository.java
+++ backend/zupzup/src/main/java/com/twoez/zupzup/pet/repository/PetQueryRepository.java
@@ -1,24 +1,9 @@
 package com.twoez.zupzup.pet.repository;
 
-import static com.twoez.zupzup.pet.domain.QPet.pet;
-
-import com.twoez.zupzup.global.querydsl.QuerydslRepositorySupport;
 import com.twoez.zupzup.pet.domain.Pet;
 import java.util.Optional;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public class PetQueryRepository extends QuerydslRepositorySupport {
+public interface PetQueryRepository {
 
-    public PetQueryRepository() {
-        super(Pet.class);
-    }
-
-    public Optional<Pet> findByMemberId(Long id) {
-
-        return Optional.ofNullable(
-                selectFrom(pet)
-                        .where(pet.member.id.eq(id).and(pet.isDeleted.isFalse()))
-                        .fetchOne());
-    }
+    Optional<Pet> findByMemberId(Long memberId);
 }
